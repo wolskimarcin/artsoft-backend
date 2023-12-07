864d9b6dab1f8c40e018c51254762a6bddb6982e
wolskimarcin <wolski.marcin.it@gmail.com>
2023-12-07 13:47:00 +0100

    Add entities

---
+++ src/main/java/com/busher/artsoftbackend/model/Address.java
@@ -0,0 +1,26 @@
+package com.busher.artsoftbackend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "address_line_1", nullable = false, length = 512)
+    private String addressLine1;
+    @Column(name = "address_line_2", length = 512)
+    private String addressLine2;
+    @Column(name = "city", nullable = false)
+    private String city;
+    @Column(name = "country", nullable = false, length = 64)
+    private String country;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private LocalUser user;
+
+}
