cd62030d832212e9075a6300c78cedecd45297f9
wolskimarcin <wolski.marcin.it@gmail.com>
2023-12-07 16:41:16 +0100

    Add validation of RegistrationBody fields

--- src/main/java/com/busher/artsoftbackend/api/model/RegistrationBody.java
+++ src/main/java/com/busher/artsoftbackend/api/model/RegistrationBody.java
@@ -1,15 +1,27 @@
 package com.busher.artsoftbackend.api.model;
 
+import jakarta.validation.constraints.*;
 
-/**
- * The information required to register a user.
- */
 public class RegistrationBody {
 
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String username;
+    @NotNull
+    @NotBlank
+    @Email
     private String email;
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d).{6,}$")
+    @Size(min = 6, max = 32)
     private String password;
+    @NotNull
+    @NotBlank
     private String firstName;
+    @NotNull
+    @NotBlank
     private String lastName;
 
     public String getUsername() {
