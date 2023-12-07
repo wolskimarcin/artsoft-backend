7db66d811c7c962976649d4ccdbe57c3e0428801
wolskimarcin <wolski.marcin.it@gmail.com>
2023-12-07 09:44:48 +0100

    Initial commit

---
+++ src/main/java/com/busher/artsoftbackend/ArtsoftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.busher.artsoftbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtsoftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtsoftBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/busher/artsoftbackend/ArtsoftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busher.artsoftbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtsoftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
