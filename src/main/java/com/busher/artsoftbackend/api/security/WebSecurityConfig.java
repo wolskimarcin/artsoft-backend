614091a7d04dce7ccc43034476f99cb7871bf1ca
wolskimarcin <wolski.marcin.it@gmail.com>
2023-12-08 09:57:54 +0100

    Add initial WebSecurityConfig

---
+++ src/main/java/com/busher/artsoftbackend/api/security/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.busher.artsoftbackend.api.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // TODO: Configure cors
+        http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
+}
