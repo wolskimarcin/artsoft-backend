98f3c4ed4632f2450ffb7ff20c95707176f8e72d
wolskimarcin <wolski.marcin.it@gmail.com>
2023-12-14 15:59:27 +0100

    Enable JWT Token authorization for Swagger-UI

--- src/main/java/com/busher/artsoftbackend/config/OpenAPIConfig.java
+++ src/main/java/com/busher/artsoftbackend/config/OpenAPIConfig.java
@@ -1,9 +1,12 @@
 package com.busher.artsoftbackend.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -40,11 +43,20 @@ public class OpenAPIConfig {
                 .title("Products Management API")
                 .version("1.0")
                 .contact(contact)
-                .description("This API exposes endpoints to manage items.").termsOfService("https://www.busher.com/terms")
+                .description("This API exposes endpoints to manage items.")
+                .termsOfService("https://www.busher.com/terms")
                 .license(mitLicense);
 
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
                 .info(info)
                 .servers(List.of(devServer, prodServer));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
