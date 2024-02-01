a23bc2ae23914f1d0eccddbc85d0eb08f7231d8c
wolskimarcin <wolski.marcin.it@gmail.com>
2024-02-01 16:54:46 +0100

    Fix security bug that allowed to generate jwt key

--- src/main/java/com/busher/artsoftbackend/service/JWTService.java
+++ src/main/java/com/busher/artsoftbackend/service/JWTService.java
@@ -2,6 +2,7 @@ package com.busher.artsoftbackend.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.busher.artsoftbackend.model.LocalUser;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -44,7 +45,8 @@ public class JWTService {
     }
 
     public String getUsername(String token) {
-        return JWT.decode(token).getClaim(USERNAME_KEY).asString();
+        DecodedJWT decodedJWT = JWT.require(algorithm).withIssuer(issuer).build().verify(token);
+        return decodedJWT.getClaim(USERNAME_KEY).asString();
     }
 
 }
--- src/test/java/com/busher/artsoftbackend/service/JWTServiceTest.java
+++ src/test/java/com/busher/artsoftbackend/service/JWTServiceTest.java
@@ -2,10 +2,14 @@ package com.busher.artsoftbackend.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.MissingClaimException;
+import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.busher.artsoftbackend.model.LocalUser;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
@@ -21,6 +25,8 @@ import static org.junit.jupiter.api.Assertions.*;
 })
 class JWTServiceTest {
 
+    @Value("${jwt.algorithm.key}")
+    private String algorithmKey;
     @Autowired
     private JWTService jwtService;
 
@@ -65,6 +71,24 @@ class JWTServiceTest {
 
         String username = jwtService.getUsername(token);
         assertEquals(testUser.getUsername(), username);
+    }
+
+    @Test
+    public void whenGetUsername_withJWTNotGeneratedByUs_thenThrowException() {
+        String token =
+                JWT.create().withClaim("USERNAME", testUser.getUsername()).sign(Algorithm.HMAC256(
+                        "fake-secret"));
+        Assertions.assertThrows(SignatureVerificationException.class,
+                () -> jwtService.getUsername(token));
+    }
+
+    @Test
+    public void whenGetUsername_withCorrectJWTButWithoutIssuer_thenThrowException() {
+        String token =
+                JWT.create().withClaim("USERNAME", testUser.getUsername())
+                        .sign(Algorithm.HMAC256(algorithmKey));
+        Assertions.assertThrows(MissingClaimException.class,
+                () -> jwtService.getUsername(token));
     }
 
 }
