2713f9a0752c748f20ff5dffef7b22267b7380ad
wolskimarcin <wolski.marcin.it@gmail.com>
2024-02-02 21:31:59 +0100

    Improve security of stored passwords by ensuring that each password is hashed with a unique salt

--- src/main/java/com/busher/artsoftbackend/service/PasswordHashingService.java
+++ src/main/java/com/busher/artsoftbackend/service/PasswordHashingService.java
@@ -1,6 +1,5 @@
 package com.busher.artsoftbackend.service;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
@@ -10,13 +9,9 @@ public class PasswordHashingService {
 
     @Value("${hashing.salt.rounds}")
     private int saltRounds;
-    private String salt;
-    @PostConstruct
-    public void postConstruct() {
-        salt = BCrypt.gensalt(saltRounds);
-    }
 
     public String hashPassword(String password) {
+        String salt = BCrypt.gensalt(saltRounds);
         return BCrypt.hashpw(password, salt);
     }
 
