367dc854b1d7947779645c94037adb8a4a5304e2
wolskimarcin <wolski.marcin.it@gmail.com>
2024-02-05 00:38:00 +0100

    Fix issue with /product path resolution

--- src/main/java/com/busher/artsoftbackend/api/controller/ProductController.java
+++ src/main/java/com/busher/artsoftbackend/api/controller/ProductController.java
@@ -20,7 +20,7 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping
+    @GetMapping("")
     public ResponseEntity<Page<Product>> getProducts(
             @RequestParam(required = false) String searchTerm,
             @RequestParam(defaultValue = "0") int page,
