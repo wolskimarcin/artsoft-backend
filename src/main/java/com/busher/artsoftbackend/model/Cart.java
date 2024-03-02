75f3994c7887d98e6df49807e820a98432b617a3
wolskimarcin <wolski.marcin.it@gmail.com>
2024-03-02 10:44:03 +0100

    Add Cart and CartItem models

---
+++ src/main/java/com/busher/artsoftbackend/model/Cart.java
@@ -0,0 +1,37 @@
+package com.busher.artsoftbackend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "cart")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> items = new HashSet<>();
+
+    public Cart(Long id) {
+        this.id = id;
+    }
+
+    public void addItem(CartItem item) {
+        items.add(item);
+        item.setCart(this);
+    }
+
+    public void removeItem(CartItem item) {
+        items.remove(item);
+        item.setCart(null);
+    }
+}
---
+++ src/main/java/com/busher/artsoftbackend/model/CartItem.java
@@ -0,0 +1,35 @@
+package com.busher.artsoftbackend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "cart_item")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public CartItem(Long id, Cart cart, Long productId, Integer quantity) {
+        this.id = id;
+        this.cart = cart;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+}
